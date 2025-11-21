aaf853417495dce776ce577f8497742a53118833
BryanChanona <233299@ids.upchiapas.edu.mx>
2025-11-21 15:28:06 -0600

    Modificacion para load_balancer

--- backend_docker/server.js
+++ backend_docker/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const booksRoutes = require('./src/routes/book.routes.js')
+
 //Cargar variables de entorno
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const app = express();
+const serverName = process.env.SERVERNAME;
 
 // Middlewares
 
@@ -16,12 +17,11 @@ app.use(express.json())
 
 
 //Ruta raiz
-app.get('/api/books/Chanona', (req, res) => {
-  res.send('Briyan de Jesús Chanona Hernández')
+app.get('/raiz', (req, res) => {
+  res.send(`${serverName}`)
 })
-//Rutas
-app.use('/api/books',booksRoutes);
+
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}, desde ${serverName}`)
 })
--- backend_docker/src/domain/Book.model.js
+++
@@ -1,9 +0,0 @@
-class Book {
-  constructor(id, title, description) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-  }
-}
-
-module.exports = Book;
